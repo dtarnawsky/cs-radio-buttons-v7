436f92d294998c2638cceef1e298c381a7c31bc0
Damian Tarnawsky <damian@ionic.io>
2023-03-07 11:17:12 -0800

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal: IonModal | undefined = undefined;
 
-  constructor() {}
+  fuel = 'i1';
+  fuelList: fuel[] = [
+    { text: 'item 1', value: 'i1' },
+    { text: 'item 2', value: 'i2' },
+    { text: 'item 3', value: 'i3' }
+  ];
+  constructor() { }
+  
+  setFuel(e: any) {
+    this.fuel = e.detail.value;
+    console.log('Set Fuel',this.fuel);
+  }
 
+  cancel() {
+    this.modal?.dismiss(null, 'cancel');
+  }
+
+}
+
+interface fuel {
+  text: string;
+  value: string;
 }
