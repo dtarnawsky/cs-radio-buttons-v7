d667d5393f6d5287f09fff951f1c1e392f52e496
Damian Tarnawsky <damian@ionic.io>
2023-03-07 10:25:42 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.radio.buttons.v7',
+  appName: 'cs-radio-buttons-v7',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
